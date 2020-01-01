d4ac10600ed89ea737fec9b79335559738f31c59
Mike <Skvm@users.noreply.github.com>
2020-01-01 22:09:32 +0000

    Delete disable_medic.py

--- disable_medic.py
+++
@@ -1,5 +0,0 @@
-import winreg
-
-key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, 'SYSTEM\\CurrentControlSet\\Services\\WaaSMedicSvc')
-winreg.SetValueEx(key, 'Start', 0, winreg.REG_SZ, '4')
-key.Close()
